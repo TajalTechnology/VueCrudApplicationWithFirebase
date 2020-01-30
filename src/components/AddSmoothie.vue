762b4c79fc6a449f9a017115f4c2fe750570a5a5
Tajal <mrpresident1189@gmail.com>
2020-01-30 03:20:18 -0800

    Firebase with vue CRUD

---
+++ src/components/AddSmoothie.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smothie Recipe</h2>
+    <form>
+      <div class="field title">
+      <label for="title">Smoothie Title:</label>
+      <input type="text" name="title" v-model="title">
+
+    </div>
+      <div class="field add-ingredient">
+        <label for="add-ingredient"> Ad an igredient:</label>
+        <input type="text" name="add-ingredient">
+
+      </div>
+      
+
+    </form>
+
+  </div>
+
+
+</template>
+
+<script>
+    export default {
+        name: "AddSmoothie",
+      data(){
+          return{
+            title: null
+
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+var firebaseConfig = {
+    apiKey: "AIzaSyDs9oQXWyzIWq_i7kVrtSsBmDxESXaXcdg",
+    authDomain: "tajaltechnology.firebaseapp.com",
+    databaseURL: "https://tajaltechnology.firebaseio.com",
+    projectId: "tajaltechnology",
+    storageBucket: "tajaltechnology.appspot.com",
+    messagingSenderId: "561200287135",
+    appId: "1:561200287135:web:1af93aa3f4e7e53ecbc1aa",
+    measurementId: "G-FNCBLRSX16"
+  };
+const firebaseApp= firebase.initializeApp(firebaseConfig);
+firebaseApp.firestore().settings( {timestampsInSnapshots: true} )
+   //firebase.analytics();
+export default firebaseApp.firestore()
